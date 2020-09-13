cebaf649841b3562f0bc2951c5ffbaf19f980760
wwoj <wojcik.wojtek@yahoo.pl>
2020-09-13 18:51:05 +0200

    FInished task

---
+++ README.md
@@ -0,0 +1,27 @@
+# algorithmic task
+
+## Test the application
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+## General info
+The purpose of this project was to create algoritm that receives two sequences: A and B of integers and returns one sequence C.
+
+## Technologies
+Project is created with:
+* JS
+* HTML
+
+## Setup
+To run this project,
+```
+$ git clone https://wojwixx@bitbucket.org/dev_practice_pl/airkiosk_ww.git or download repository
+$ open file and runt the index.html
+```
+
+## Contact
+Created by @Wojciech Wojcik - feel free to contact me!
+
---
+++ index.js
@@ -0,0 +1,71 @@
+const A = [2, 3, 9, 2, 5, 1, 3, 7, 10];
+const B = [2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10];
+var C = [];
+window.onload = function() {
+    var h1 = document.createElement("h1");
+    document.body.appendChild(h1);
+    h1.innerText = `Array A: [${C}]`;
+    var h1 = document.createElement("h1");
+    document.body.appendChild(h1);
+    h1.innerText = `Array B: [${B}]`;
+    var h3 = document.createElement("h1");
+    document.body.appendChild(h3);
+    h3.innerText = `Array C: [${C}]`;
+
+    document.body.appendChild(ul);
+   
+
+  };
+
+function primeTest(N) {
+  if (N <= 1) {
+    return true;
+  } else {
+    var liczba = 0;
+    for (let i = 1; i <= N / 2; i++) {
+      if (N % i === 0) {
+        liczba++;
+      }
+      if (liczba >= 2) {
+        break;
+      }
+    }
+    if (liczba >= 2) {
+      return true;
+    }
+    return false;
+  }
+}
+
+function generateCArray(A, B) {
+  for (let j = 0; j < A.length; j++) {
+    if (checkInC(A[j], j)) {
+      if (C.includes(A[j])) {
+        C.push(A[j]);
+        continue;
+      }
+    }
+
+    let counts = 0;
+    B.forEach((elem) => {
+      if (elem === A[j]) {
+        counts++;
+      }
+    });
+
+    if (primeTest(counts)) {
+      C.push(A[j]);
+    }
+  }
+}
+
+function checkInC(value, index) {
+  for (let i = 0; i < index; i++) {
+    if (A[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
+generateCArray(A, B);
+console.log(C);
